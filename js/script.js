a52017c850419bd03930a9bceee417a4b2f134cf
TomasBelfonte <tomas.belfonte@gmail.com>
2022-11-30 16:45:03 +0100

    Email-list

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const { createApp } = Vue
+
+const app = createApp({
+    data() {
+        return {
+            message: ''
+        }
+    },
+    methods: {
+        recoveryMail() {
+            // effettuo una richiesta axios al server per ricevere un dato esterno 
+            // e da un server esterno. Nello specifico ricevo una lista email.
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            // questa funzione parte quando axios riceve la risposta dal server
+            .then(function(response){
+            // la risposta è contenuta nell'argomento della funzione che nello specifico chiamiamo - response.
+
+            // verifico il contenuto della isposta e verifico il funzionamento. 
+            console.log(response);
+            });
+        },
+
+    },
+    mounted(){
+    // qui devo invocate la funzione - recoveriMail per farla funzionare
+    this.recoveryMail()
+
+    },
+}).mount('#app')
